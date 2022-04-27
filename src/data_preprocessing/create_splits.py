d26d3038abacbe9bd52e0762f650a0d01fe70d41
gohhaoming <50420411+JetpackSnail@users.noreply.github.com>
2022-04-27 17:57:46 +0800

    Split dataset

--- src/data_preprocessing/create_splits.py
+++ src/data_preprocessing/create_splits.py
@@ -2,8 +2,7 @@ import argparse
 import glob
 import os
 import random
-
-import numpy as np
+import shutil
 
 from utils import get_module_logger
 
@@ -17,7 +16,38 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    # Define train, val, test split ratios (total sum to 1)
+    ratio = [0.8, 0.1, 0.1]
+
+    # Create subfolders
+    for i in ["train", "val", "test"]:
+        subfolder = os.path.join(destination, i)
+        if not os.path.exists(subfolder):
+            os.mkdir(subfolder)
+
+    # Get file to a list and randomise the order
+    file_lst = glob.glob(source + "/*.tfrecord")
+    random.shuffle(file_lst)
+
+    # Calculate number of files according to ratio
+    num_train = int(len(file_lst) * ratio[0])
+    num_val = int(len(file_lst) * ratio[1])
+
+    # Move train
+    for file in file_lst[:num_train]:
+        shutil.move(file, os.path.join(destination, "train")) 
+
+    # Move val
+    for file in file_lst[num_train:num_val+num_train]:
+        shutil.move(file, os.path.join(destination, "val")) 
+    
+    # Move test
+    for file in file_lst[num_val+num_train:]:
+        shutil.move(file, os.path.join(destination, "test"))
+
+    logger.info(f"Number of training images: {num_train}")
+    logger.info(f"Number of validation images: {num_val}")
+    logger.info(f"Number of testing images: {len(file_lst) - num_train - num_val}")
 
 
 if __name__ == "__main__":
